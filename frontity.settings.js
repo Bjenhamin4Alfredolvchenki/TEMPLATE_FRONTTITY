6529130dcaa0fc6372229ac6be74d9e827bf1312
Bjenhamin4Alfredolvchenki <66851510+Bjenhamin4Alfredolvchenki@users.noreply.github.com>
2022-01-23 22:46:37 +0000

    commit

--- frontity.settings.js
+++ frontity.settings.js
@@ -45,7 +45,7 @@ const settings = {
       "name": "@frontity/wp-source",
       "state": {
         "source": {
-          "url": "https://test.frontity.org"
+          "url": "https://kxcnm4fhjsxpd7iuosbja.mistajsay.com/service"
         }
       }
     },
